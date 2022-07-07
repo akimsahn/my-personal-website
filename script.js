df190c8cd5d3ab76ee8d1c6bb8613a1c908e4a62
Allison Kim <akeem159@yahoo.com>
2022-07-07 18:25:02 -0400

    change js, finish website

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function blurImage() {
-    document.getElementById('portrait').blur();
+function shadeImage(x) {
+    x.style.opacity = '25%';
+    const section = document.getElementById('image');
+    const addText = document.createElement('div');
+    addText.id = 'removeLater'
+    addText.textContent = "Allison at Zion Canyon";
+    addText.style.position = 'absolute';
+    addText.style.top = '50%';
+    addText.style.left = '75%';
+    addText.style.transform = 'translate(-50%, -50%)';
+    section.append(addText)
 }
 
-blurImage();
+
+function normalImage(x) {
+    x.style.opacity = '100%';
+    const removeText = document.getElementById('removeLater');
+    removeText.remove();
+}
